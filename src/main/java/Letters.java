c396d25aecb326c85efb1c351c36be8bcc58bb74
jensschlegel <jens.b.schlegel@gmail.com>
2020-06-24 18:52:52 +0200

    removed0

--- src/main/java/Letters.java
+++ src/main/java/Letters.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class Letters {
     public static void main(String[] args) {
-        
-        String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", };
+
+        String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                            "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1",
+                            "2", "3", "4", "5", "6", "7", "8", "9"};
 
         String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
                 ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
-                "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
-                "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
-                "-----", "--..--", ".-.-.-", "..--.."};
+                "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----",
+                "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
 
         Scanner sc = new Scanner(System.in);
-        String  input = sc.nextLine().toLowerCase() + 0;
+        String  input = sc.nextLine().toLowerCase();
+        System.out.println(input);
         int len = input.length();
         String result = "";
 
