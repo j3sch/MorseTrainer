c791906cb2f29a8f1a28a5cc8c8e0f3781ec8ef6
jensschlegel <jens.b.schlegel@gmail.com>
2020-06-28 23:33:35 +0200

    done translator, beginned input

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -6,6 +6,45 @@ public class Input {
         Scanner sc = new Scanner(System.in);
         String  input = sc.nextLine().toLowerCase();
 
+        boolean morsecode = false;
+        boolean morseAndLettes = false;
+        int len = input.length();
+
+        if (len == 0) {
+            System.out.println("No Input");
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (input.substring(i, i+1).equals(".") || input.substring(i, i+1).equals("-")) {           //Checkt, ob input Morse oder Letters sind
+                morsecode = true;
+            } else {
+                morseAndLettes = true;
+            }
+            if (morsecode && morseAndLettes) {
+                System.out.println("Error, only morse or only letters and numbers!");
+
+            }
+            if (!morsecode) {                                                                         //Word -> Morsecode
+                for (int j = 0; j < letters.length; j++) {
+                    if (input.substring(i, i+1).equals(letters[j])) {
+                        result += morse[j];
+                        break;
+                    }
+                    if (j == letters.length - 1) {
+                        System.out.println("Error, only morse or only letters and numbers!");
+                    }
+                }
+            }
+            /* if (morsecode) {
+                for (int j = 0; j < morse.length; j++) {
+                    if (input.substring(i, i+1).equals(morse[j])) {
+                        result += letters[j];
+                        break;
+                    }
+                }
+            }
+             */
+        }
 
 
 
--- src/main/java/Translator.java
+++ src/main/java/Translator.java
@@ -13,48 +13,17 @@ public class Translator {
                           ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..",
                           "----."};
 
-
-        int len = input.length();
+        int len = word.length();
         String result = "";
-        boolean morsecode = false;
-        boolean morseAndLettes = false;
-
-        if (len == 0) {
-            System.out.println("No Input");
-        }
 
         for (int i = 0; i < len; i++) {
-            if (input.substring(i, i+1).equals(".") || input.substring(i, i+1).equals("-")) {           //Checkt, ob input Morse oder Letters sind
-                morsecode = true;
-            } else {
-                morseAndLettes = true;
-            }
-            if (morsecode && morseAndLettes) {
-                System.out.println("Error, only morse or only letters and numbers!");
-
-            }
-            if (!morsecode) {                                                                         //Word -> Morsecode
-                for (int j = 0; j < letters.length; j++) {
-                    if (input.substring(i, i+1).equals(letters[j])) {
-                        result += morse[j];
-                        break;
-                    }
-                    if (j == letters.length - 1) {
-                        System.out.println("Error, only morse or only letters and numbers!");
-                    }
+            for (int j = 0; j < letters.length; j++) {
+                if (word.substring(i, i+1).equals(letters[j])) {
+                    result += morse[j];
+                    break;
                 }
             }
-            /* if (morsecode) {
-                for (int j = 0; j < morse.length; j++) {
-                    if (input.substring(i, i+1).equals(morse[j])) {
-                        result += letters[j];
-                        break;
-                    }
-                }
-            }
-             */
         }
-        System.out.println(result);
+        return result;
     }
-
 }
