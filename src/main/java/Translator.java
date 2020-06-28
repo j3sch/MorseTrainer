00acca9a75e906dfe9e22c2074629d10c629b746
jensschlegel <jens.b.schlegel@gmail.com>
2020-06-28 22:59:31 +0200

    added query only morse or only letters and translator morsecode to word

--- src/main/java/Translator.java
+++ src/main/java/Translator.java
@@ -8,27 +8,52 @@ public class Translator {
                             "2", "3", "4", "5", "6", "7", "8", "9"};
 
         String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
-                ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
-                "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----",
-                "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+                          ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
+                          "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----",
+                          ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..",
+                          "----."};
 
         Scanner sc = new Scanner(System.in);
         String  input = sc.nextLine().toLowerCase();
-        System.out.println(input);
         int len = input.length();
         String result = "";
+        boolean morsecode = false;
+        boolean morseAndLettes = false;
 
         if (len == 0) {
             System.out.println("No Input");
         }
 
         for (int i = 0; i < len; i++) {
-            for (int j = 0; j < letters.length; j++) {
-                if (input.substring(i, i+1).equals(letters[j])) {
-                    result += morse[j];
-                    break;
+            if (input.substring(i, i+1).equals(".") || input.substring(i, i+1).equals("-")) {           //Checkt, ob input Morse oder Letters sind
+                morsecode = true;
+            } else {
+                morseAndLettes = true;
+            }
+            if (morsecode && morseAndLettes) {
+                System.out.println("Error, only morse or only letters and numbers!");
+                break;
+            }
+            if (!morsecode) {                                                                         //Word -> Morsecode
+                for (int j = 0; j < letters.length; j++) {
+                    if (input.substring(i, i+1).equals(letters[j])) {
+                        result += morse[j];
+                        break;
+                    }
+                    if (j == letters.length - 1) {
+                        System.out.println("Error, only morse or only letters and numbers!");
+                    }
                 }
             }
+            /* if (morsecode) {
+                for (int j = 0; j < morse.length; j++) {
+                    if (input.substring(i, i+1).equals(morse[j])) {
+                        result += letters[j];
+                        break;
+                    }
+                }
+            }
+             */
         }
         System.out.println(result);
     }
