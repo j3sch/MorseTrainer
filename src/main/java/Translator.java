f4374db08a6f0e9b4ffd451818b4b31c9c292bbb
jensschlegel <jens.b.schlegel@gmail.com>
2020-07-03 17:56:54 +0200

    letters and morse moved to Class

--- src/main/java/Translator.java
+++ src/main/java/Translator.java
@@ -4,24 +4,25 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 public class Translator {
+
+    static char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1',
+            '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    static String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+            ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
+            "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----",
+            "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+
     public static String abcToMorse(String word) {
 
-        String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1",
-                "2", "3", "4", "5", "6", "7", "8", "9"};
-
-        String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
-                ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
-                "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----",
-                "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
-        
         int len = word.length();
         StringBuilder result = new StringBuilder();
         char[] wordArray = word.toCharArray();
 
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < letters.length; j++) {
-                if (wordArray[i].equals(letters[j])) {
+                if (wordArray[i] == letters[j]) {
                     result.append(morse[j]);
                     break;
                 }
