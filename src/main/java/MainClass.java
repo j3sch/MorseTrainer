a5357f5c18d1562fc16d720f10f3cf890400d5fe
coffemakingtoaster <mh340@hdm-stuttgart.de>
2020-07-14 12:44:04 +0200

    Implemented new Method in UI...yet to be tested as translator branch has to be finished before executing the software is possible

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -19,22 +19,23 @@ public class MainClass extends Application {
     @Override
     public void start(Stage stage) {
         main_UI_grid.setSpacing(8);
+        main_UI_grid.setAlignment(Pos.CENTER);
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label head_label = new Label("Select prefered gamemode");
         Button M2L_Button = new Button();
         Button L2M_Button = new Button();
-        main_UI_grid.getChildren().addAll(head_label,M2L_Button,L2M_Button);
         L2M_Button.setText("Letters -> Morse");
         M2L_Button.setText("Morse -> letters");
         M2L_Button.setOnAction(e->{
                 main_UI_grid.getChildren().clear();
-                game_screen(0, stage);
+                default_game_screen(1, stage);
         });
         L2M_Button.setOnAction(e->{
                 main_UI_grid.getChildren().clear();
-                game_screen(1, stage);
+                default_game_screen(0, stage);
         });
+        main_UI_grid.getChildren().addAll(head_label,M2L_Button,L2M_Button);
         stage.setTitle("Morsetrainer");
         stage.setResizable(false);
         stage.setScene(scene);
@@ -42,41 +43,44 @@ public class MainClass extends Application {
     }
 
     public static void main(String[] args) {
-
         launch();
     }
 
-    public void game_screen(Integer game_mode, Stage stage){
-        final String[] data = {"THIS IS A HUGE PLACEHOLDER!!!!!!","... .... ..- -- -... ..- .-.."};
+    public void default_game_screen(Integer game_mode, Stage stage){
+        final String[] data = MorseQuiz.defaultGameWord();
         final String usr_txt_inpt;
         //get data from backend logic
-        System.out.println(game_mode);
         final HBox tmp = new HBox();
-        final Label given_value = new Label(data[0]);
-        main_UI_grid.getChildren().add(given_value);
+        final Label given_value = new Label(data[game_mode]);
         Button return_to_MM_button = new Button("Main Menu");
-        return_to_MM_button.setOnAction(e->{return_to_Main_Menu(stage);});
-        tmp.getChildren().addAll(given_value,return_to_MM_button);
-        tmp.setSpacing(10);
+        tmp.setAlignment(Pos.CENTER);
         HBox.setMargin(tmp,new Insets(20,20,20,20));
+        tmp.setSpacing(10);
+        tmp.getChildren().addAll(given_value,return_to_MM_button);
+        return_to_MM_button.setOnAction(e->{return_to_Main_Menu(stage);});
+        //main_UI_grid.getChildren().add(given_value);
         main_UI_grid.getChildren().add(tmp);
-        if (game_mode==0){
+        // Morse -> Alpha
+        if (game_mode==1){
             final HBox user_input = new HBox();
             final TextField alpha_input = new TextField();
             final Button user_done = new Button();
             user_done.setText("Done");
             user_done.setOnAction(e->{
                     String alpha_from_user = alpha_input.getCharacters().toString();
-                    if (alpha_from_user.equals(data[1])){
-                        game_screen(game_mode,stage);
+                    if (alpha_from_user.equals(data[0])){
+                        main_UI_grid.getChildren().clear();
+                        default_game_screen(game_mode,stage);
                     }else{
                         popup_wrong_answer();
                     }
             });
+            user_input.setAlignment(Pos.CENTER);
             user_input.getChildren().addAll(alpha_input,user_done);
             main_UI_grid.getChildren().add(user_input);
         }
-        else if (game_mode == 1){
+        // Alpha -> Morse
+        else if (game_mode == 0){
             HBox user_input = new HBox();
             final Label display_user_input = new Label();
             display_user_input.setTextFill(Color.web("#0099ff"));
@@ -89,12 +93,14 @@ public class MainClass extends Application {
             Button done_button = new Button();
             done_button.setText("done");
             done_button.setOnAction(e-> {
-                if(display_user_input.getText().equals(data[0])){
-                    game_screen(game_mode,stage);
+                if(display_user_input.getText().equals(data[1])){
+                    main_UI_grid.getChildren().clear();
+                    default_game_screen(game_mode,stage);
                 } else {
                     display_user_input.setText("");
                     popup_wrong_answer();
                 }});
+            user_input.setAlignment(Pos.CENTER);
             main_UI_grid.getChildren().add(display_user_input);
             user_input.getChildren().addAll(short_morse,long_morse,done_button);
             main_UI_grid.getChildren().add(user_input);
@@ -115,6 +121,45 @@ public class MainClass extends Application {
         popup_window.setScene(popup_scene);
         popup_window.showAndWait();
     }
+
+    public void choices_gamemode(Integer gamemode){
+        if (gamemode == 0) {
+            String[] game_contents = MorseQuiz.getWordToMorseQuiz();
+        }
+        else{
+            String[] game_contents = MorseQuiz.getMorseToWordQuiz();
+        }
+        final HBox tmp = new HBox();
+        final Label given_word = new Label(game_contents[0]);
+        Button return_to_MM_button = new Button("Main Menu");
+        tmp.setAlignment(Pos.CENTER);
+        HBox.setMargin(tmp,new Insets(20,20,20,20));
+        tmp.setSpacing(10);
+        tmp.getChildren().addAll(given_word,return_to_MM_button);
+        return_to_MM_button.setOnAction(e->{return_to_Main_Menu(stage);});
+        Integer amount_of_created_buttons = 0;
+        HBox button_row = new HBox();
+        while (amount_of_created_buttons<4){
+            int random_index = (int)(Math.random() * (4 - 1 + 1) + 1);
+            if (game_contents[random_index] != "in use"){
+                Button choice = new Button(game_contents[random_index]);
+                if (random_index==1){
+                    choice.setOnAction(e-> {
+                        main_UI_grid.getChildren().clear();
+                        choices_gamemode(gamemode);
+                        });
+                    }
+                else{
+                    choice.setOnAction(e->{
+                        choice.setEnabled(false);
+                    });
+                }
+                choice.getChildren().add(done_button);
+                amount_of_created_buttons+=1;
+            }
+        }
+    }
+
 
     public void return_to_Main_Menu(Stage stage){
         main_UI_grid.getChildren().clear();
