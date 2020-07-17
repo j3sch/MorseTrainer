6b7a097ba0d47784730324ce24d88aeeb4a0246b
jensschlegel <jens.b.schlegel@gmail.com>
2020-07-17 13:17:03 +0200

    added some junit tests

--- src/test/java/TranslatorTest.java
+++ src/test/java/TranslatorTest.java
@@ -18,24 +18,39 @@ public class TranslatorTest {
     // Jens Tests
     // todo: write tests for your stuff
     @Test public void test_abcToMorse_words() {
-        Assert.assertEquals("-. ..- .-.. .-..", Translator.abcToMorse("null"));
+        Assert.assertEquals("", Translator.abcToMorse(""));
+        Assert.assertEquals(".-- . .-.. -.-. --- -- .", Translator.abcToMorse("welcome"));
         Assert.assertEquals(".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..", Translator.abcToMorse("abcdefghijklmnopqrstuvwxyz"));
         Assert.assertEquals("----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----.", Translator.abcToMorse("0123456789"));
-        Assert.assertEquals(".... .- ? .-.. .-.. ? ---", Translator.abcToMorse("ha/ll-o"));
+        Assert.assertEquals("--..-- .-.-.- ..--.. -.-.--", Translator.abcToMorse(",.?!"));
+        Assert.assertEquals(".... . ? .-.. .-.. ? ---", Translator.abcToMorse("he/ll-o"));
+        Assert.assertEquals("--- -. .-.. -.-- ? .-- --- .-. -.. ... ? .-- .. .-.. .-.. ? - .-. .- -. ... .-.. .- - . -..", Translator.abcToMorse("only words will translated"));
 
     }
 
     @Test public void test_abcToMorse_sentence() {
-        Assert.assertEquals("-.. .- ... / .... .. . .-. / .. ... - / . .. -. / - . ... - -.-.--", Translator.abcToMorse("das hier ist ein test!".toCharArray()));
-
+        Assert.assertEquals("", Translator.abcToMorse("".toLowerCase().toCharArray()));
+        Assert.assertEquals("/ / / / / / / / / / ", Translator.abcToMorse("          ".toLowerCase().toCharArray()));
+        Assert.assertEquals("- .... .. ... / .. ... / .- / - . ... - -.-.--", Translator.abcToMorse("This is a test!".toLowerCase().toCharArray()));
+        Assert.assertEquals("- .... . / - .-. .- -. ... .-.. .- - .. --- -. / ..-. . . / .. ... / ...-- -.... ..... --..-- ----. ---.. / . ..- .-. --- ...", Translator.abcToMorse("The translation fee is 365,98 Euros".toLowerCase().toCharArray()));
+        Assert.assertEquals("..- -. .- -.-. -.-. . .--. - .- -... .-.. . / -.-. .... .- .-. .- -.-. - . .-. ... / .-.. .. -.- . / ? ? ? ? ? ? / .-- .. .-.. .-.. / -... . / -.. .. ... .--. .-.. .- -.-- . -.. /" +
+                " .- ... / --.- ..- . ... - .. --- -. " + "/ -- .- .-. -.- ...", Translator.abcToMorse("unacceptable characters like -;#<>* will be displayed as question marks".toLowerCase().toCharArray()));
     }
 
     @Test public void test_morseToAbc_word() {
-        Assert.assertEquals("null", Translator.morseToAbc("-. ..- .-.. .-.."));
+        Assert.assertEquals("?", Translator.morseToAbc(""));
+        Assert.assertEquals("welcome", Translator.morseToAbc(".-- . .-.. -.-. --- -- ."));
+        Assert.assertEquals("abcdefghijklmnopqrstuvwxyz", Translator.morseToAbc(".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.."));
+        Assert.assertEquals("0123456789", Translator.morseToAbc("----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----."));
+        Assert.assertEquals(",.?!", Translator.morseToAbc("--..-- .-.-.- ..--.. -.-.--"));
+        Assert.assertEquals("?", Translator.morseToAbc("hello"));
+        Assert.assertEquals("only?words?will?translated", Translator.morseToAbc("--- -. .-.. -.-- / .-- --- .-. -.. ... / .-- .. .-.. .-.. / - .-. .- -. ... .-.. .- - . -.."));
+
     }
 
     @Test public void test_morseToAbc_sentence() {
-
+    Assert.assertEquals("?", Translator.morseToAbc(""));
+    Assert.assertEquals("welcome", Translator.morseToAbc(".-- . .-.. -.-. --- -- ."));
     }
     // End of Jens Test
 
